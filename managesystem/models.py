c35d4cee06fbf2264462d9456c0c87c3aff305bd
southcold <744648584@qq.com>
2019-12-03 10:30:32 +0800

    引用静态文件

--- managesystem/models.py
+++ managesystem/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=15, primary_key=True)
     password = models.CharField(max_length=30)
     isadmin = models.IntegerField(default=0)
